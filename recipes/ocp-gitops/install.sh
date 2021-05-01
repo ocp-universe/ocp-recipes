625ae6f97df235831fa2b51edf88f489aa5c3ff3
Hafid Haddouti <code@haddouti.com>
2021-05-01 21:45:15 +0200

    New configs for Elasticsearch, Jaeger, Kiali, ServiceMesh and ArgoCD updates

--- recipes/ocp-gitops/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Generic install scripts, executes all resources in the right order
-
-#../../install.sh $(dirname "${BASH_SOURCE[0]}")
-../../install.sh $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
